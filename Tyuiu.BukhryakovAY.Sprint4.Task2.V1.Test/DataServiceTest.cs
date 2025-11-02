820c748857138df958b9239194d49b4c85646451
Артем Бухряков <artembuhryakov332@gmail.com>
2025-11-02 23:08:01 +0500

    Task2

---
+++ Tyuiu.BukhryakovAY.Sprint4.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BukhryakovAY.Sprint4.Task2.V1.Lib;
+namespace Tyuiu.BukhryakovAY.Sprint4.Task2.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            int[] arrayNum = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+            int wait = 3072;
+            int res = dataService.Calculate(arrayNum);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BukhryakovAY.Sprint4.Task2.V1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BukhryakovAY.Sprint4.Task2.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        DataService dataService = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       **");
+        Console.WriteLine("***************************************************************************");
+        int lenght = 10;
+        int[] arrayNum = new int[lenght];
+        for(int i = 0; i < arrayNum.Length; i++)
+        {
+            arrayNum[i] = rnd.Next(1,10);
+        }
+        Console.WriteLine("Получим массив: " + String.Join(",", arrayNum));
+        int res = dataService.Calculate(arrayNum);
+        Console.WriteLine("Произведение всех чисел, кратных 2: " + res);
+    }
+}
