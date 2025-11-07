d4aa3e31d29ea69e6ab71d202d79680d55f8d184
Артем Бухряков <artembuhryakov332@gmail.com>
2025-11-07 21:47:38 +0500

    Task5

---
+++ Tyuiu.BukhryakovAY.Sprint4.Task5.V29.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿
+using Tyuiu.BukhryakovAY.Sprint4.Task5.V29.Lib;
+namespace Tyuiu.BukhryakovAY.Sprint4.Task5.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            int[,] matrix = {{ -9, 6, -9, -3, 7 },
+                             { 3, -3, -3, -8, 2 },
+                             { -2, -1, -3, -5, 2 },
+                             { -6, -2, -3, -2, -5 },
+                             { 4, 5, -6, -9, -5 } };
+            int res = dataService.Calculate(matrix);
+            int wait = 7;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.BukhryakovAY.Sprint4.Task5.V29/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.BukhryakovAY.Sprint4.Task5.V29.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        DataService dataService = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       **");
+        Console.WriteLine("***************************************************************************");
+        int[,] matrix = new int[5, 5];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = random.Next(-9, 10);
+            }
+        }
+        Console.WriteLine("Дан массив:");
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Console.WriteLine(" ");
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + ",");
+            }
+        }
+        int res = dataService.Calculate(matrix);
+        Console.WriteLine(" ");
+        Console.WriteLine("Количество положительных элементов в матрице: " + res);
+    }
+}
